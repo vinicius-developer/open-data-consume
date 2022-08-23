78d87d0da949aadbab0a5a9ea41ecd4445d326d8
vinicius-developer <viniciusilvav1@hotmail.com>
2022-08-23 19:20:51 +0000

    first commit

---
+++ src/main/java/br/com/open/data/consume/App.java
@@ -0,0 +1,13 @@
+package br.com.open.data.consume;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
